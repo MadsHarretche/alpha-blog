6aa574b69c1cfea4c2846b723535c3699f819d84
Mads Harretche <madsharretche@outlook.com>
2021-01-20 14:37:17 +0000

    DRY - made som changes to the code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,11 @@
 class ArticlesController < ApplicationController
-    
+    #before_action kalder set_article på alle metoder dvs. @article = Article.find(params[:id])
+    #Men kun de metoder som er skrevet i only:
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
     def show
-        @article = Article.find(params[:id])
+      #finder id'et på den artikel du vil handle med
+      set_article
     end
     
     def index
@@ -12,8 +16,12 @@ class ArticlesController < ApplicationController
       @article = Article.new
     end
     
+    def edit
+    end
+    
+    
     def create
-      @article = Article.new(params.require(:article).permit(:title, :description))      
+      @article = Article.new(article_params)
       if @article.save
         flash[:notice] = "Article was created successfully!"
         redirect_to article_path(@article)
@@ -22,6 +30,34 @@ class ArticlesController < ApplicationController
       else
         render 'new'
       end
+    end
+    
+    def update
+      # => .update har samme effekt som .save. params.require, whitelister title og description så det kan bruges.
+      if @article.update(article_params)
+        #Giver besked efter man har trykket update
+        flash[:notice] = "Article was updated successfully!"
+        redirect_to article_path(@article)
+      else
+        render 'edit'
+      end
+    
+    end
+    
+    def destroy
+      @article.destroy
+      redirect_to articles_path
+    end
+    
+    #private means that any method I put below it, are available only to this controller
+    private
+    
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    
+    def article_params
+      params.require(:article).permit(:title, :description) 
     end
     
 end
