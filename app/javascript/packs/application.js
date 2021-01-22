ee634a3bd554105138e39136da802e6e80ebccf6
Mads Harretche <madsharretche@outlook.com>
2021-01-22 22:52:12 +0000

    CSS on everysite. Thanks bootstrap.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//nedenstående fire linjer har jeg selv sat ind, da de også stod der på kurset.
+//require("@rails/ujs").start()
+//require("turbolinks").start()
+//require("@rails/activestorage").start()
+//require("channels").start()
+
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "bootstrap"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+
 
 Rails.start()
 Turbolinks.start()
