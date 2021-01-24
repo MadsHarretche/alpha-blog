c42ac3974bb2972035a1e9de62ad1c27f53b1220
Mads Harretche <madsharretche@outlook.com>
2021-01-24 22:22:39 +0000

    Preparing user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+#self referer hver enkelt objekt af user class, så når det kommer til email-feltet, så vil et enhvert objekt
+#blive downcased inden det bliver saved
+    before_save { self.email = email.downcase }
     has_many :articles  
     validates :username, presence:true, 
                         uniqueness: { case_sensitive: false }, 
@@ -8,4 +11,5 @@ class User < ApplicationRecord
                         uniqueness: { case_sensitive: false }, 
                         length: { maximum: 105 },
                         format: { with: VALID_EMAIL_REGAX }
+has_secure_password
 end
