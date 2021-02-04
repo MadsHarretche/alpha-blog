0e002c765dbe050a2e27d9166ef37f3eb4435ad5
Mads Harretche <madsharretche@outlook.com>
2021-02-04 11:50:47 +0000

    prøver lige igen

--- db/migrate/20210202134114_create_articles.rb
+++ db/migrate/20210202134114_create_articles.rb
@@ -1,8 +1,8 @@
-#class CreateArticles < ActiveRecord::Migration[6.1]
- # def change
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
   #  create_table :articles do |t|
   ##   t.text :description
     #  t.timestamps
     #end
-  #end
-#end
+  end
+end
