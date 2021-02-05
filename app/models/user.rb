bb722d0c7e8935e251e935a3435a6b500b740bae
Mads Harretche <madsharretche@outlook.com>
2021-02-05 13:40:39 +0000

    Delete user function is now available

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 #self referer hver enkelt objekt af user class, så når det kommer til email-feltet, så vil et enhvert objekt
 #blive downcased inden det bliver saved
     before_save { self.email = email.downcase }
-    has_many :articles  
+    has_many :articles, dependent: :destroy #any articles where user is detroyed, articles will be destroyed as well
     validates :username, presence:true, 
                         uniqueness: { case_sensitive: false }, 
                         length: { minimum: 3, maximum: 25 }
