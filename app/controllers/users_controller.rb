56e5224f4154e78a6bfcea4f83a743b48c1511b9
Mads Harretche <madsharretche@outlook.com>
2021-02-05 12:26:44 +0000

    More restrictions in url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+  
   
   
   def show
@@ -53,4 +56,13 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
+
+  def require_same_user
+      if current_user != @user
+      flash[:alert] = "You cannot edit others profile"
+        redirect_to @user
+      end
+    end
+
+
 end
