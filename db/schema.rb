1601c64d610960334d10d27ec45bbb5a9c0c1dbd
Mads Harretche <madsharretche@outlook.com>
2021-02-09 23:22:18 +0000

    add categories to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_180602) do
+ActiveRecord::Schema.define(version: 2021_02_09_191150) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
