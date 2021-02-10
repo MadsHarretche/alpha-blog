fd4912e605b9f586d9669e21b08bb017ea995631
Mads Harretche <madsharretche@outlook.com>
2021-02-10 15:20:52 +0000

    pagination on users index now works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @user = User.all
+    @users = User.all
     @users = User.paginate(page: params[:page], per_page: 5)
   end
   
