a3cb734d205bae2c602298e22ea1090469257454
Mads Harretche <madsharretche@gmail.com>
2021-07-11 17:07:54 +0200

    s

--- db/migrate/20210212131103_add_reads_to_articles.rb
+++ db/migrate/20210212131103_add_reads_to_articles.rb
@@ -1,5 +1,5 @@
 class AddReadsToArticles < ActiveRecord::Migration[6.1]
   def change
-    add_column :articles, :reads, :interger, default: 0
+    add_column :articles, :reads, :integer, default: 0
   end
 end
